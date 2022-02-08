932f7165457131300928cba5434a338f4a6fe426
Wojtek Jarosz <wojtek.jarosz@jorsek.com>
2022-02-08 18:26:13 +0100

    Tagster Extension in React - 1st ver

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  React.useEffect(() => {
+    // TODO this is how you send an event from react app. Can remove
+    // chrome.tabs && chrome.tabs.query({
+    //   active: true,
+    //   currentWindow: true
+    // }, tabs => {
+    //   chrome.tabs.sendMessage(
+    //       tabs[0].id || 0,
+    //       {type: 'UPDATE_TAGS'} as UpdateTagsMessage,
+    //       (response: UpdateTagsResponse) => {
+    //         console.log("Got response: " + response)
+    //       });
+    // });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Login buttons and maybe some settings here.
+          </p>
+        </header>
+      </div>
   );
 }
 
